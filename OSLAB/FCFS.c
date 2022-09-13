0f6c421f9eb94f5456a08ab2fa9f344aef77d304
Mounika Meenuga <94857308+204G1A0561@users.noreply.github.com>
2022-09-13 12:16:36 +0530

    Create FCFS.c

---
+++ OSLAB/FCFS.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: M.Mounika
+   Roll No: 204G1A0561
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
