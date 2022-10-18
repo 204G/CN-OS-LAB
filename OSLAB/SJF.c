7b9b6f4260e7167dfa5145e1fbe76afe2bedc411
204G1A0561 <204G1A0540@srit.ac.in>
2022-10-18 12:32:54 +0530

    successfully executed sjfout1

--- OSLAB/SJF.c
+++ OSLAB/SJF.c
@@ -5,14 +5,11 @@
    Date of Execution: 06-09-2022
 */
 #include<stdio.h>
-#include<conio.h>
-main()
+int main()
 {
 int p[20], bt[20], wt[20], tat[20], i, k, n, temp; float wtavg,
 tatavg;
-clrscr();
 printf("\nEnter the number of processes -- ");
-
 scanf("%d", &n);
 for(i=0;i<n;i++)
 {
@@ -44,5 +41,5 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n); 
-getch();
+return 0;
 }
