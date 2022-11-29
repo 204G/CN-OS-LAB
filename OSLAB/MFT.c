6978c90beb65fbb28fc9fd8cc91a2fb97e34cffd
204G1A0561 <204G1A0561@srit.ac.in>
2022-11-29 12:24:01 +0530

    First list of Errors rectified.

--- OSLAB/MFT.c
+++ OSLAB/MFT.c
@@ -5,12 +5,10 @@
    Date of Execution: 29-11-2022
 */
 #include<stdio.h>
-#include<conio.h>
-main()
+int main()
 {
 int ms, bs, nob, ef,n,
 mp[10],tif=0; int i,p=0;
-clrscr();
 printf("Enter the total memory available (in Bytes) -- ");
 scanf("%d",&ms);
 printf("Enter the block size (in Bytes) -- ");
@@ -25,8 +23,7 @@ printf("Enter memory required for process %d (in Bytes)-- ",i+1);
 scanf("%d",&mp[i]);
 }
 printf("\nNo. of Blocks available in memory--%d",nob);
-printf("\n\nPROCESS\tMEMORYREQUIRED\tALLOCATED\tINTERNAL
-FRAGMENTATION");
+printf("\n\nPROCESS\tMEMORYREQUIRED\tALLOCATED\tINTERNALFRAGMENTATION");
 for(i=0;i<n && p<nob;i++)
 {
 printf("\n %d\t\t%d",i+1,mp[i]);
@@ -36,7 +33,6 @@ else
 {
 printf("\t\tYES\t%d",bs-mp[i]);
 tif = tif + bs-mp[i];
-
 p++;
 }
 }
@@ -44,5 +40,5 @@ if(i<n)
 printf("\nMemory is Full, Remaining Processes cannot be accomodated");
 printf("\n\nTotal Internal Fragmentation is %d",tif);
 printf("\nTotal External Fragmentation is %d",ef);
-getch();
+return 0;
 }
